bf4f4393f2d14a076bf3a4bb33c2ef9abc13653c
Katherine Daugherty <katherinedaugherty23@gmail.com>
2021-09-09 16:04:32 -0600

    answereda couple of questions- first draft

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,31 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket =[]; //global variable set to empty
+
+function addItem(item){  //function with a string 'item'
+  basket.push('item');  //add item to basket array
+  return true;
+}
+console.log(addItem('item'));
+console.log(`basket contains: ${basket}`);
+
+
+
+
+function listItems(){
+  for ( let i = 0; i <= basket.length; i++){
+    console.log(basket[i]);
+  }
+}
+console.log('In the basket we have:', listItems());
+
+
+
+
+function empty(){
+  basket = [];
+  return 'empty';
+}
+console.log(empty());
